ecc2e03aa299b7d05cf534debf92226a0be813d0
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 06:27:22 +0530

    Added: Sum of n natural numbers

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -1,3 +1,4 @@
+//Write a program for the sum of n natural number ex. Input N = 5 output 1+2+3+4+5
 package com.bridgelabz;
 
 import java.util.Scanner;
@@ -5,32 +6,22 @@ import java.util.Scanner;
 public class PracticeProblem {
 
     public static void main(String[] args) {
-        //Values taken for each digit
-        int number;
-        int unit ;
-        int tens;
-        int hundreds;
-        int thousands;
-
-        //Creating new Scanner class
+        //Creating Scanner class for taking input from the user
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the 4 digit Number:");
-        number = sc.nextInt();
+        System.out.println("Enter Number:");
+        int number = sc.nextInt();
+        int i = 1;
+        int sum = 0;
 
-        unit = number % 10;
-        System.out.println("The number is in unit place: " + unit);
-        tens = number % 100 / 10;
-        System.out.println("The number is in tens place: " + tens);
-        hundreds = number % 1000 / 100;
-        System.out.println("The number is in hundreds place: " + hundreds);
-        thousands = number % 10000 / 1000;
-        System.out.println("The number is in thousands place:" + thousands);
-
-        if (number > 9999) {
-            System.out.println("Error! Number more then 4 digits.");
-        } else {
-            System.out.println("Exit.");
+        //while loop will execute the condition until it returns true value
+        while (i <= number) {
+            //adding value of i into the sum variable
+            sum = sum + i;
+            //incrementing value of i by 1
+            i++;
         }
+        //Displaying sum of n natural numbers
+        System.out.println("Sum of n Natural Numbers is: " + sum);
     }
 }
 
