fd4c6406eeda1fdac98c36e8b0c835b0ec40cddf
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 06:50:05 +0530

    Added: To check whether alphabet is vowel or consonant

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -6,23 +6,28 @@ import java.util.Scanner;
 public class PracticeProblem {
 
     public static void main(String[] args) {
-            int number, temp, sum, remainder;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter any number: ");
-            number = sc.nextInt();
-            temp = number;
-
-            for (sum = 0; number != 0; number/=10) {
-                remainder = number % 10;
-                sum = (sum * 10)+ remainder;
-            }
-            //palindrome if number and sum are equal
-            if (sum == temp) {
-                System.out.println(temp+ " is a palindrome");
-            } else {
-                System.out.println(temp+" is not a palindrome");
-            }
+        char ch;
+        //Scanner class takes input from the user
+        Scanner sc = new Scanner(System.in);
+        //get input and it is stored ch variable
+        System.out.println("Enter any Alphabet: ");
+        ch = sc.next().charAt(0);
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
         }
     }
-
+}
 
