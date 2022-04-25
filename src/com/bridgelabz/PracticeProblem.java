7949baa31be68abfe658f46c865c57a7804eac4f
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 06:01:53 +0530

    Added: Display unit ten and hundred

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -3,36 +3,34 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class PracticeProblem {
+
     public static void main(String[] args) {
-
+        //Values taken for each digit
         int number;
-        System.out.println("Enter the single digit");
-        Scanner sc = new Scanner(System.in);
-        number = sc.nextInt();
-        sc.close();
+        int unit ;
+        int tens;
+        int hundreds;
+        int thousands;
 
-        if ((number == 0)) {
-            System.out.println("ZERO");
-        } else if ((number == 1)) {
-            System.out.println("ONE");
-        } else if ((number == 2)) {
-            System.out.println("TWO");
-        } else if ((number == 3)) {
-            System.out.println("THREE");
-        } else if ((number == 4)) {
-            System.out.println("FOUR");
-        } else if ((number == 5)) {
-            System.out.println("FIVE");
-        } else if ((number == 6)) {
-            System.out.println("SIX");
-        } else if ((number == 7)) {
-            System.out.println("SEVEN");
-        } else if ((number == 8)) {
-            System.out.println("EIGHT");
-        } else if ((number == 9)) {
-            System.out.println("NINE");
-        } else
-            System.out.println("Enter a single digit");
+        //Creating new Scanner class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 4 digit Number:");
+        number = sc.nextInt();
+
+        unit = number % 10;
+        System.out.println("The number is in unit place: " + unit);
+        tens = number % 100 / 10;
+        System.out.println("The number is in tens place: " + tens);
+        hundreds = number % 1000 / 100;
+        System.out.println("The number is in hundreds place: " + hundreds);
+        thousands = number % 10000 / 1000;
+        System.out.println("The number is in thousands place:" + thousands);
+
+        if (number > 9999) {
+            System.out.println("Error! Number more then 4 digits.");
+        } else {
+            System.out.println("Exit.");
+        }
     }
 }
 
